46c7a95008ba00bd4a13aeeb7f7aee84dd5f9c99
Tyson Burghardt <tyson.burghardt@gmail.com>
2016-03-21 21:33:58 +0100

    making progress

--- edfparser.py
+++ edfparser.py
@@ -1,15 +1,18 @@
 import module io
-import datetime as dt
+import dfatetime as dt
+import numpy as np
 
-def edfparse(filename):
+def edfparse(filename, edfsubtype="default"):
   eegrawf = io.open(filename, 'rb')
   beeg = io.BufferedReader(eegrawf)
 
-  eegstuff = parseHdr(str(beeg.read(256)))
+  eegstuff = parseHdr(beeg.read(256))
+  eegstuff["sigdata"] = parsesighdrs(
+                beeg.read(eegstuff["hdrbytes"]-256),
+                eegstuff["ns"])
 
-  ns = eegstuff["ns"]
-  
-  labels = 
+  beeg.seek(eegstuff["hdrbytes"])
+  eegstuff["data"] = parsesignals(beeg.read(-1), eegstuff)
 
   beeg.close()
 
@@ -29,7 +32,8 @@ def parseHdr(bb):
             "epoch":epoch,
             "ndr":ndr,
             "dur":dur_dr,
-            "ns":ns}
+            "ns":ns,
+            "hdrbytes":hdrbytes}
 
 
 
@@ -50,5 +54,54 @@ def parserecinfo(bb):
             "eegnum"  : eegnum,
             "techcode"  : techcode,
             "equip" : equipcode }
+
+
+def getit(x, a, b):
+  return x[a:a+b]
+
+
+def parsesighdrs(bb, i):
+  lbloff = 0
+  troff = i*16
+  phdimoff = i*(16+80)
+  phminoff = i*(16+80+8)
+  phmaxoff = i*(16+80+8+8)
+  digminoff = i*(16+80+8+8+8)
+  digmaxoff = i*(16+80+8+8+8+8)
+  prefoff =  i*(16+80+8+8+8+8+8)
+  nsampoff =  i*(16+80+8+8+8+8+8+80)
+  jj = [dict() for i in range(i)]
+  for j in range(i):
+    jj[j]["label"] = str(getit(bb, lbloff, 8).rstrip()
+    jj[j]["trans_type"] = str(getit(bb, troff, 80))
+    jj[j]["ph_dim"] = str(getit(bb, phdimoff, 8)).rstrip()
+    jj[j]["ph_min"] = float(getit(bb, phminoff, 8).rstrip())
+    jj[j]["ph_max"] = float(getit(bb, phmaxoff, 8).rstrip())
+    jj[j]["dig_min"] = int(getit(bb, digminoff, 8).rstrip())
+    jj[j]["dig_max"] = int(getit(bb, digmaxoff, 8).rstrip())
+    jj[j]["prefilt"] = str(getit(bb, prefoff, 80))
+    jj[j]["nsamp"] = int(geti(bb, nsampoff, 8).rstrip())
+
+  return jj
+    
+
+
+
+def parsesignals(bb, ss):
+
+#nsigs, nrec, nsamps):
+  ns = ss["ns"]
+  sigs = np.array((ns,max(ss["sigdata"][m]["nsamps"] for m in range[ns])), dtype=np.float)
+  drecsize = sum(nsamps)
+  for i in range(nsigs):
+    ll = 0
+    for j in range(nrec):
+      for k in range(nsamps[i]):
+        sigs[i][ll] = int.from_bytes(
+            bb[drecsize*j+k:drecsize*j+k+1], 
+            'little',
+            signed=True)/float(2**15-1)
+        ll += 1
+  return sigs
 
 
