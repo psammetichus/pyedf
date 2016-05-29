e3bbc78fdc073510842ab5c0dca9f1cae579dc92
Tyson Burghardt <tb@manchru.net>
2016-05-29 10:43:26 -0400

    added transformations and .decode()s to edfparser

--- edfparser.py
+++ edfparser.py
@@ -14,7 +14,7 @@ def edfparse(filename, edfsubtype="default"):
   beeg = io.BufferedReader(eegrawf)
 
   eegstuff = EDFEEG()
-  eegstuff.header = parseHdr(beeg.read(256))
+  eegstuff.header = parseHdr(beeg.read(256), edfsubtype)
   eegstuff.sigdata = parsesighdrs(
                 beeg.read(eegstuff.header.hdrbytes-256),
                 eegstuff.header.ns)
@@ -34,16 +34,22 @@ class Header:
     self.ndr = None
     self.dur = None
     self.ns = None
+    self.other = None
 
 
 
-def parseHdr(bb):
+def parseHdr(bb, parsestyle):
   h = Header()
   h.version = int(bb[0:8].rstrip())
-  h.demo = parseptinfo(bb[8:88])
-  h.recinfo = parserecinfo(bb[88:168])
-  h.epoch = dt.datetime.strptime(bb[168:184],
-        "%d.%m.%y%h.%M.%S")
+  if parsestyle == 'default':
+      h.demo = parseptinfo(bb[8:88])
+      h.recinfo = parserecinfo(bb[88:168])
+  else:
+      h.demo = ""
+      h.recinfo = ""
+      h.other = bb[8:168].decode()
+  h.epoch = dt.datetime.strptime(bb[168:184].decode(),
+        "%d.%m.%y%H.%M.%S")
   h.hdrbytes = int(bb[184:192])
   h.ndr = int(bb[236:244])
   h.dur = int(bb[244:252])
@@ -59,23 +65,23 @@ class Demographics:
 
 
 def parseptinfo(bb):
-  [mrn, sex, bday, name] = bb.split(" ")
-  dob = dt.datetime.strptime(bday, "%d-%b-%Y").date()
-  lname, gname = name.split("_")
+  [mrn, sex, bday, name, *rest] = bb.split(b" ")
+  dob = dt.datetime.strptime(bday.decode(), "%d-%b-%Y").date()
+  lname, gname = name.split(b"_")
   d = Demographics()
-  d.name = (lname, gname)
-  d.sex = sex
-  d.dob = dob
-  d.mrn = mrn
+  d.name = (lname.decode(), gname.decode())
+  d.sex = sex.decode()
+  d.dob = dob.decode()
+  d.mrn = mrn.decode()
   return d
 
 def parserecinfo(bb):
-  [stmark, stdate, eegnum, techcode, equipcode] = bb.split(" ")
-  recdate = dt.datetime.strptime(stdate, "%d-%b-%Y").date()
-  return {  "recdate" : recdate,
-            "eegnum"  : eegnum,
-            "techcode"  : techcode,
-            "equip" : equipcode }
+  [stmark, stdate, eegnum, techcode, equipcode, *rest] = bb.split(b" ")
+  recdate = dt.datetime.strptime(stdate.decode(), "%d-%b-%Y").date()
+  return {  "recdate" : recdate.decode(),
+            "eegnum"  : eegnum.decode(),
+            "techcode"  : techcode.decode(),
+            "equip" : equipcode.decode() }
 
 
 def getit(x, a, b):
@@ -105,15 +111,15 @@ def parsesighdrs(bb, i):
   nsampoff =  i*(16+80+8+8+8+8+8+80)
   jj = [ChannelInfo() for i in range(i)]
   for j in range(i):
-    jj[j].label = str(getit(bb, lbloff, 8).rstrip()
-    jj[j].trans_type = str(getit(bb, troff, 80))
-    jj[j].ph_dim = str(getit(bb, phdimoff, 8)).rstrip()
+    jj[j].label = getit(bb, lbloff, 8).rstrip().decode()
+    jj[j].trans_type = getit(bb, troff, 80).decode()
+    jj[j].ph_dim = getit(bb, phdimoff, 8).rstrip().decode()
     jj[j].ph_min = float(getit(bb, phminoff, 8).rstrip())
     jj[j].ph_max = float(getit(bb, phmaxoff, 8).rstrip())
     jj[j].dig_min = int(getit(bb, digminoff, 8).rstrip())
     jj[j].dig_max = int(getit(bb, digmaxoff, 8).rstrip())
-    jj[j].prefilt = str(getit(bb, prefoff, 80))
-    jj[j].nsamp = int(geti(bb, nsampoff, 8).rstrip())
+    jj[j].prefilt = getit(bb, prefoff, 80).decode()
+    jj[j].nsamp = int(getit(bb, nsampoff, 8).rstrip())
 
   return jj
     
@@ -121,23 +127,30 @@ def parsesighdrs(bb, i):
 def storeit(sig, off, i, k):
   sig[i,off[i]] = k
 
+def transform(qty, dmin, dmax, phmin, phmax):
+    qq = (qty-dmin)/float(dmax-dmin)
+    return qq*(phmax-phmin)+phmin
+
+def tx_by_sig(qty, sigdata, i):
+    return transform(qty, sigdata[i].dig_min, sigdata[i].dig_max,
+                            sigdata[i].ph_min, sigdata[i].ph_max)
 
 def parsesignals(bb, ss):
   ns = ss.header.ns
-  nsamps = [ss.sigdata[k].nsamp for k in range[ns]
-  sigs = np.array((ns,max(nsamps)*ss.header.ndr), dtype=np.float)
+  nsamps = [ss.sigdata[k].nsamp for k in range(ns)]
+  sigs = np.zeros( (ns, max(nsamps)*ss.header.ndr), dtype=np.float)
   drecsize = sum(nsamps)
-
   offsets = [0 for i in range(ns)]
-  k= 0
-  for i in range(ndr):
+  buffers = [np.zeros( nsamps[i], dtype=np.int16) for i in range(ns)]
+
+  #loop over drecs
+  for i in range(ss.header.ndr):
+    k = i*drecsize
+    #loop over sig chunks in a drec
     for j in range(ns):
-      sigs[i][offsets[i]]
-      m = i*nsamps[j]*2
-      storeit(sigs, offsets, j, int.from_bytes(
-            bb[k:m+1],
-            'little',
-            signed=True)/float(2**15-1))
+      m = i*drecsize + nsamps[j]*2
+      buffers[j] = np.frombuffer(bb[k:m+1], dtype=np.int16)
+      storeit(sigs, offsets, j, tx_by_sig(buffers[j], ss.sigdata, j))
       offsets[j] = nsamps[j]
       k = m
   return sigs
